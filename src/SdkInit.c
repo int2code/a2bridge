5950e8c9db264a0a178aaa98988f924cc93cb575
Rafal Brewka <rafal.brewka.ext@qorix.ai>
2026-01-19 17:36:39 +0000

    Add Sdk Init callback

---
+++ src/SdkInit.c
@@ -0,0 +1,13 @@
+/**
+ * @file SdkInit.c
+ * @brief SDK specific system initialization
+ *
+ *
+ */
+
+#include "Log.h"
+
+void Sdk_Init(void)
+{
+    LOG_INFO("SDK specific initialization");
+}
--- src/main.c
+++
@@ -1,7 +0,0 @@
-// extern void core_main();
-
-// int main()
-// {
-//     core_main();
-//     return 0;
-// }
